1e3ccf79cde7a18d675ef42cb053cf380f39e5e6
Boris Bošković <boris.boskovic92@gmail.com>
2022-08-14 12:35:16 +0200

    Added cascade delete on User and Post models

--- script.ts
+++ script.ts
@@ -1,13 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function main() {
-    await prisma.user.create({
-        data: { email: "boris.boskovic92@gmail.com", name: "Boris Bošković" },
-    });
-    const user = await prisma.user.findMany();
-    console.log("🎯 >>> user", user);
-}
+async function main() {}
 
 main()
     .catch((err) => {
